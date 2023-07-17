786808d2060f0a88698d5dc2b51f6cb6e02c11b5
Philipp <philipp.seith@gmx.net>
2023-07-17 22:36:03 +0200

    new: subject impl

--- pkg/rx/subject.go
+++ pkg/rx/subject.go
@@ -1,1 +1,54 @@
 package rx
+
+import (
+	"sync"
+
+	"golang.org/x/exp/slices"
+)
+
+type Subject[T any] struct {
+	observers []Observer[T]
+	mx        sync.RWMutex
+}
+
+func (s *Subject[T]) Subscribe(o Observer[T]) Subscription {
+	s.mx.Lock()
+	defer s.mx.Unlock()
+
+	s.observers = append(s.observers, o)
+	return &subscription{u: func() {
+		s.mx.Lock()
+		defer s.mx.Unlock()
+
+		if idx := slices.Index(s.observers, o); idx != -1 {
+			s.observers = append(s.observers[:idx], s.observers[idx+1:]...)
+		}
+	}}
+}
+
+func (s *Subject[T]) Next(value T) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+
+	for _, o := range s.observers {
+		o.Next(value)
+	}
+}
+
+func (s *Subject[T]) Error(err error) {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+
+	for _, o := range s.observers {
+		o.Error(err)
+	}
+}
+
+func (s *Subject[T]) Complete() {
+	s.mx.RLock()
+	defer s.mx.RUnlock()
+
+	for _, o := range s.observers {
+		o.Complete()
+	}
+}
