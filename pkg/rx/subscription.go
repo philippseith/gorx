86cbb46ee10bed6f764705c741999d68e7d52db2
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2023-07-19 06:53:47 +0200

    new: NewSubscription

--- pkg/rx/subscription.go
+++ pkg/rx/subscription.go
@@ -4,6 +4,10 @@ type Subscription interface {
 	Unsubscribe()
 }
 
+func NewSubscription(unsubscribe func()) Subscription {
+	return &subscription{u: unsubscribe}
+}
+
 type subscription struct {
 	u func()
 }
