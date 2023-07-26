e70fc00b9818f13de40af366334d777980518161
Philipp <philipp.seith@gmx.net>
2023-07-26 22:37:05 +0200

    chg: just test

--- pkg/rx_test/from_just_test.go
+++ pkg/rx_test/from_just_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/philippseith/gorx/pkg/rx"
 )
 
-func TestJust(t *testing.T) {
+func TestFrom(t *testing.T) {
 	expected := []int{1, 2, 3}
 	f := rx.From(expected...)
 	var actual []int
@@ -17,4 +17,15 @@ func TestJust(t *testing.T) {
 	}, nil, nil))
 
 	assert.Equal(t, expected, actual)
+}
+
+func TestJust(t *testing.T) {
+	expected := 314
+	f := rx.Just(expected)
+	var actual []int
+	f.Subscribe(rx.NewObserver[int](func(i int) {
+		actual = append(actual, i)
+	}, nil, nil))
+
+	assert.Equal(t, expected, actual[0])
 }
