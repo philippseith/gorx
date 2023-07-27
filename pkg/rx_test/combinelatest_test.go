6496415845605f98501f88f9c18648d951d58017
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2023-07-27 11:39:00 +0200

    chg: remove flaky CombineLatest test with ticker

--- pkg/rx_test/combinelatest_test.go
+++ pkg/rx_test/combinelatest_test.go
@@ -1,9 +1,7 @@
 package rx_test
 
 import (
-	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -29,18 +27,42 @@ func TestCombineLatest(t *testing.T) {
 	assert.Equal(t, []any{'B', 3}, actual)
 }
 
-func TestCombineLatestTicker(t *testing.T) {
-	for i := 0; i < 10; i++ {
-		// 1499 and 1999 are prime
-		t1 := rx.NewTicker(0, 1499*time.Microsecond)
-		t2 := rx.NewTicker(0, 1999*time.Microsecond)
-		tt1 := rx.Scan[time.Time, int](rx.Take[time.Time](t1, 3), func(i int, t time.Time) int { return i + 1 }, 0)
-		tt2 := rx.Scan[time.Time, int](rx.Take[time.Time](t2, 3), func(i int, t time.Time) int { return i + 1 }, 0)
-
-		sl := rx.ToSlice(context.Background(), rx.CombineLatest[[]any](func(ts ...any) []any { return ts }, rx.ToAny(tt1), rx.ToAny(tt2)))
-		// the second and forth combination are not stable
-		assert.Equal(t, []any{1, 1}, sl[0])
-		assert.Equal(t, []any{2, 2}, sl[2])
-		assert.Equal(t, []any{3, 3}, sl[4])
-	}
-}
+//func TestCombineLatestTicker(t *testing.T) {
+//	for i := 0; i < 1; i++ {
+//		// 149 and 199 are prime
+//		t1 := rx.NewTicker(100*time.Millisecond, 1*time.Microsecond)
+//		ch1 := make(chan int, 1)
+//		ch2 := make(chan int, 1)
+//		j := 0
+//		t1.Subscribe(rx.OnNext[time.Time](func(time.Time) {
+//			j++
+//			//fmt.Println(j)
+//			if j == 6 {
+//				close(ch1)
+//				close(ch2)
+//			}
+//			if j >= 6 {
+//				return
+//			}
+//			ch1 <- j
+//			if j%2 == 0 {
+//				ch2 <- j
+//			}
+//		}))
+//		tt1 := rx.Scan[int, int](rx.FromChan[int](ch1), func(i int, k int) int {
+//			//fmt.Println("A", i, " ", k)
+//			return i + 1
+//		}, 0)
+//		tt2 := rx.Scan[int, int](rx.FromChan[int](ch2), func(i int, k int) int {
+//			//fmt.Println("B", i, " ", k)
+//			return i + 1
+//		}, 0)
+//
+//		sl := <-rx.ToSlice[[]any](rx.CombineLatest[[]any](func(ts ...any) []any { return ts }, rx.ToAny[int](tt1), rx.ToAny[int](tt2)))
+//
+//		for _, s := range sl {
+//			fmt.Println(s)
+//		}
+//		assert.Equal(t, [][]any{{2, 1}, {2, 2}, {3, 2}, {4, 2}, {5, 2}}, sl)
+//	}
+//}
