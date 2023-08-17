1f5ef6a5c12c30815cc5ca5a7b344d201f4c8550
Philipp <philipp.seith@gmx.net>
2023-08-17 20:28:49 +0200

    fix: wrong Debounce behavior

--- pkg/rx/debounce.go
+++ pkg/rx/debounce.go
@@ -8,43 +8,35 @@ import (
 // DebounceTime returns an Observable that delays the emissions of the source
 // Subscribable by the specified duration and may drop some values if they occur
 // too frequently.
-// TODO Behavior is not correct. It should emit after time, not when the first goes in after that time
 func DebounceTime[T any](s Subscribable[T], duration time.Duration) Observable[T] {
-	d := &debounceTime[T]{
-		Operator: Operator[T, T]{t2u: func(t T) T { return t }},
-		duration: duration,
-		last:     time.Unix(0, 0),
-	}
-	d.SubscribeToSource(d, s)
-	return ToObservable[T](d)
+	return Debounce[T, time.Time](s, NewTicker(0, duration))
 }
 
-type debounceTime[T any] struct {
-	Operator[T, T]
-	duration time.Duration
-	last     time.Time
-}
-
-func (d *debounceTime[T]) Next(value T) {
-	if time.Since(d.last) > d.duration {
-		d.last = time.Now()
-		d.Operator.Next(value)
-	}
-}
-
+// Debounce emits a notification from the source Observable only after a
+// particular time span has passed without another source emission
 func Debounce[T any, U any](s Subscribable[T], trigger Subscribable[U]) Observable[T] {
 	d := &debounce[T]{
 		Operator: Operator[T, T]{t2u: func(t T) T { return t }},
 	}
 
 	triggerSub := trigger.Subscribe(OnNext(func(U) {
-		d.mx.RLock()
-		defer d.mx.RUnlock()
+		if func() bool {
+			d.mx.RLock()
+			defer d.mx.RUnlock()
 
-		if d.hasLast {
-			d.hasLast = false
-			d.Next(d.last)
+			return d.hasLast
+		}() {
+			func() {
+				d.mx.Lock()
+				defer d.mx.Unlock()
+
+				d.hasLast = false
+			}()
+			if o := d.getObserver(); o != nil {
+				o.Next(d.last)
+			}
 		}
+
 	}))
 	d.SubscribeToSource(d, s)
 	ds := ToObservable[T](d)
