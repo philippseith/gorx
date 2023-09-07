f92f6699c2dcd7bf0db33571dbcff9fbfdda3800
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2023-09-07 12:47:16 +0200

    chg: rx.Go mor like go func() {}()

--- pkg/rx/go.go
+++ pkg/rx/go.go
@@ -2,21 +2,26 @@ package rx
 
 import "context"
 
-func Go[T any](ctx context.Context, f func() (T, error)) (T, error) {
-	select {
-	case <-ctx.Done():
-		r := Result[T]{}
-		return r.Ok, ctx.Err()
-	case r := <-func() <-chan Result[T] {
-		ch := make(chan Result[T])
-		go func() {
-			r := Result[T]{}
-			r.Ok, r.Err = f()
+func Go[T any](ctx context.Context, f func() (T, error)) <-chan Result[T] {
+	ch := make(chan Result[T], 1)
+	go func() {
+		defer close(ch)
+
+		select {
+		case <-ctx.Done():
+			ch <- Err[T](ctx.Err())
+		case r := <-func() <-chan Result[T] {
+			chf := make(chan Result[T])
+			go func() {
+				r := Result[T]{}
+				r.Ok, r.Err = f()
+				chf <- r
+				close(chf)
+			}()
+			return chf
+		}():
 			ch <- r
-			close(ch)
-		}()
-		return ch
-	}():
-		return r.Ok, r.Err
-	}
+		}
+	}()
+	return ch
 }
--- pkg/rx_test/go_test.go
+++ pkg/rx_test/go_test.go
@@ -10,26 +10,26 @@ import (
 )
 
 func TestGo(t *testing.T) {
-	x, err := rx.Go(context.Background(), func() (int, error) {
+	x := <-rx.Go(context.Background(), func() (int, error) {
 		return 1, nil
 	})
 
-	assert.Equal(t, 1, x)
-	assert.NoError(t, err)
+	assert.Equal(t, 1, x.Ok)
+	assert.NoError(t, x.Err)
 
-	y, err := rx.Go(context.Background(), func() (float64, error) {
+	y := <-rx.Go(context.Background(), func() (float64, error) {
 		return 1.0, errors.New("fail")
 	})
 
-	assert.Equal(t, 1.0, y)
-	assert.Error(t, err)
+	assert.Equal(t, 1.0, y.Ok)
+	assert.Error(t, y.Err)
 
 	ctx, cancel := context.WithCancel(context.Background())
 	cancel()
-	z, err := rx.Go(ctx, func() (string, error) {
+	z := <-rx.Go(ctx, func() (string, error) {
 		return "z", nil
 	})
 
-	assert.Equal(t, "", z)
-	assert.Equal(t, ctx.Err(), err)
+	assert.Equal(t, "", z.Ok)
+	assert.Equal(t, ctx.Err(), z.Err)
 }
