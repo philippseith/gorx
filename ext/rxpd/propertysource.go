5bbcea85f89cad697e079fc6788abb3ae053a825
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2023-11-30 07:42:43 +0000

    new: PropertySource

---
+++ ext/rxpd/propertysource.go
@@ -0,0 +1,28 @@
+package rxpd
+
+import "github.com/philippseith/gorx/pkg/rx"
+
+// PropertySource creates and stores Property objects by IDs that can be reused.
+type PropertySource[ID comparable, T any] struct {
+	properties     map[ID]Property[T]
+	createProperty func(ID) Property[T]
+}
+
+// NewPropertySource creates a PropertySource which creates Property objects for IDs with the createProperty function.
+func NewPropertySource[ID comparable, T any](createProperty func(ID) Property[T]) *PropertySource[ID, T] {
+	return &PropertySource[ID, T]{
+		properties:     map[ID]Property[T]{},
+		createProperty: createProperty,
+	}
+}
+
+// GetProperty returns the Property object for an ID. The Property has ShareReplay behavior with MaxBufferSize 1.
+// This means each new subscriber will get the last value of the Property, if there is any.
+func (ps *PropertySource[ID, T]) GetProperty(id ID) Property[T] {
+	if property, ok := ps.properties[id]; ok {
+		return property
+	}
+	property := ps.createProperty(id).ShareReplay(rx.MaxBufferSize(1), rx.RefCount(true))
+	ps.properties[id] = property
+	return property
+}
