691c901952b714558ae8c8ca7d88f286a2083c74
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2023-12-04 11:14:28 +0000

    new: rxpd tap options

--- ext/rxpd/property.go
+++ ext/rxpd/property.go
@@ -1,6 +1,7 @@
 package rxpd
 
 import (
+	"log"
 	"time"
 
 	"github.com/philippseith/gorx/pkg/rx"
@@ -53,9 +54,56 @@ func WithRead[T any](read func() rx.ResultChan[T]) func(*propertyOption[T]) {
 	}
 }
 
+func WithTapRead[T any](tapRead func(func() rx.ResultChan[T]) rx.ResultChan[T]) func(*propertyOption[T]) {
+	return func(po *propertyOption[T]) {
+		poRead := po.read
+		po.read = func() rx.ResultChan[T] {
+			return tapRead(poRead)
+		}
+	}
+}
+
 func WithWrite[T any](write func(T) <-chan error) func(*propertyOption[T]) {
 	return func(po *propertyOption[T]) {
 		po.write = write
+	}
+}
+
+func WithTapWrite[T any](tapWrite func(T, func(T) <-chan error) <-chan error) func(*propertyOption[T]) {
+	return func(po *propertyOption[T]) {
+		poWrite := po.write
+		po.write = func(t T) <-chan error {
+			return tapWrite(t, poWrite)
+		}
+	}
+}
+
+func WithLogReadWrite[T any](id string) func(*propertyOption[T]) {
+	return func(po *propertyOption[T]) {
+		poRead := po.read
+		po.read = func() rx.ResultChan[T] {
+			log.Printf("Before Read %s", id)
+			ch := make(chan rx.Result[T])
+			go func() {
+				result := <-poRead()
+				log.Printf("After Read %s: %v", id, result)
+				ch <- result
+				close(ch)
+			}()
+			return ch
+		}
+		po.write = func(t T) <-chan error {
+			poWrite := po.write
+			log.Printf("Before Write %s: %v", id, t)
+			ch := make(chan error)
+			go func() {
+				err := <-poWrite(t)
+				log.Printf("After Write %s: %v", id, err)
+				ch <- err
+				close(ch)
+			}()
+			return ch
+		}
 	}
 }
 
