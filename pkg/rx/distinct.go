e595b1374649e4f33ce002c37959a9af614e925f
Philipp Seith (DC-AE/ESW1) <philipp.seith@boschrexroth.de>
2024-01-09 13:37:39 +0000

    fix:  Race in DistinctUntilChanged

--- pkg/rx/distinct.go
+++ pkg/rx/distinct.go
@@ -1,6 +1,9 @@
 package rx
 
-import "reflect"
+import (
+	"reflect"
+	"sync"
+)
 
 // Distinct returns an Observable that emits all items emitted by the source
 // Subscribable that are distinct by comparison from previous items.
@@ -23,16 +26,22 @@ func Distinct[T comparable](s Subscribable[T]) Observable[T] {
 // result observable emitted.
 func DistinctUntilChanged[T any](s Subscribable[T], equal func(T, T) bool) Observable[T] {
 	var last *T
+	var mx sync.Mutex
+
 	d := &Operator[T, T]{t2u: func(t T) T { return t }}
 	if equal == nil {
 		equal = func(a, b T) bool { return reflect.DeepEqual(a, b) }
 	}
 	d.prepareSubscribe(func() Subscription {
 		return s.Subscribe(NewObserver[T](func(value T) {
+			mx.Lock()
+			defer mx.Unlock()
+
 			if last == nil || !equal(*last, value) {
 				d.Next(value)
 			}
 			last = &value
+
 		}, d.Error, d.Complete))
 	})
 	return ToObservable[T](d)
